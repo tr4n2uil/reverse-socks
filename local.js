daa7067e38a3dbba0aa50dd5e6576624ea1bf903
Vibhaj Rajan <vibhaj8@gmail.com>
2016-03-18 05:44:55 +0530

    local byte code forwarding

--- local.js
+++ local.js
@@ -12,14 +12,28 @@ server.on('connected', function(req, dest) {
 
 var connected = false;
 
+var forwardLocal = function(buffer){
+  var sock = net.createConnection(local.port, local.host, function() {
+    sock.write(buffer);
+  })
+}
+
 var tunnel = net.createConnection(remote.port, remote.host, function() {
   console.log("Connected to tunnel", remote);
   tunnel.write(new Buffer([0x01, 0x03]))
+  var buff = null;
   tunnel.on('data', function(buffer){
     console.log("Data", buffer);
-    var sock = net.createConnection(local.port, local.host, function() {
-      sock.write(buffer);
-    })
+    var i=0;
+    if(buff && buffer.length > 0){
+      forwardLocal(new Buffer([buff, buffer[0]]));
+      i++;
+      buff = null;
+    } 
+    for(; i+1<buffer.length; i+=2){
+      forwardLocal(new Buffer([buffer[i], buffer[i+1]]));  
+    }
+    if(i+1 != buffer.length) buff = buffer[i];
   })
 }).once('error', function(err) {
   console.log("Error: ", err)
